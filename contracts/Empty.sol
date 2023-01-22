10c3b608921250f91f72520f14e6746721377d87
sodamnfoolish <sodamnfoolish@outlook.com>
2023-01-22 21:15:30 +0200

    refactored

--- contracts/Empty.sol
+++ contracts/Empty.sol
@@ -2,4 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-contract Empty {}
+contract Empty {
+    constructor() {}
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,20 @@
-import {HardhatUserConfig} from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 require("dotenv").config();
 
 const config: HardhatUserConfig = {
-	solidity: "0.8.17",
-	networks: {
-		mainnet: {
-			chainId: 1,
-			url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}`,
-			accounts: [process.env.PRIVATE_KEY!],
-		},
-	},
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY!,
-	},
+  solidity: "0.8.17",
+  networks: {
+    mainnet: {
+      chainId: 1,
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY!,
+  },
 };
 
 export default config;
